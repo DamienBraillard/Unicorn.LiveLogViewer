b676544dd0d3c08e8305ccb6af4b1099317fc0a3
Cédric Luthi <cedric.luthi@gmail.com>
2024-07-06 11:19:41 +0200

    Enable code quality analysis and fix warnings (#3)

--- src/Unicorn.LiveLogViewer/LogViewerOptions.cs
+++ src/Unicorn.LiveLogViewer/LogViewerOptions.cs
@@ -1,4 +1,4 @@
-﻿using Microsoft.Extensions.FileProviders;
+using Microsoft.Extensions.FileProviders;
 using Unicorn.LiveLogViewer.StaticContent;
 
 namespace Unicorn.LiveLogViewer;
@@ -11,6 +11,15 @@ public class LogViewerOptions
     /// <summary>
     /// Defines the <see cref="IFileProvider"/> that will be used to serve static content.
     /// </summary>
+    public LogViewerOptions()
+    {
+        var staticRootType = typeof(StaticFileNames);
+        StaticContentProvider = new EmbeddedFileProvider(staticRootType.Assembly, staticRootType.Namespace);
+    }
+
+    /// <summary>
+    /// Defines the collection of <see cref="IFileProvider"/> that will be used to locate static content.
+    /// </summary>
     /// <remarks>
     /// To replace a static file, use the <see cref="CompositeFileProvider"/> by inserting your own file provider before the default one.
     /// <code>
@@ -21,5 +30,5 @@ public class LogViewerOptions
     /// });
     /// </code>
     /// </remarks>
-    public IFileProvider StaticContentProvider { get; set; } = new EmbeddedFileProvider(typeof(StaticFiles).Assembly, typeof(StaticFiles).Namespace);
+    public IFileProvider StaticContentProvider { get; set; }
 }
