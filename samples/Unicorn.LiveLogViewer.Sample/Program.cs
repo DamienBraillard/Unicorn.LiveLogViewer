13e1ff155cdddca0583df75e4ff912a77ccd0367
Damien Braillard <contact@damien-braillard.ch>
2025-01-05 17:13:53 +0100

    Generate fake log events in test project

--- samples/Unicorn.LiveLogViewer.Sample/Program.cs
+++ samples/Unicorn.LiveLogViewer.Sample/Program.cs
@@ -1,5 +1,11 @@
+using System;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
 using Unicorn.LiveLogViewer;
 
 var builder = WebApplication.CreateSlimBuilder(args);
@@ -7,6 +13,8 @@ var builder = WebApplication.CreateSlimBuilder(args);
 // Adds the required dependencies
 builder.Services.AddHttpLogging(_ => { });
 builder.Services.AddLiveLogViewer( /*optionsBuilder: (provider, options) => {  } */);
+
+builder.Services.AddHostedService<EmitSampleLogsWorker>();
 
 var app = builder.Build();
 
@@ -17,4 +25,28 @@ app.MapLiveLogViewer( /*basePath: "/logViewer"*/);
 
 app.UseHttpLogging();
 
+
 app.Run();
+
+
+class EmitSampleLogsWorker(ILoggerFactory loggerFactory) : BackgroundService
+{
+    protected override async Task ExecuteAsync(CancellationToken stoppingToken)
+    {
+        var random = new Random();
+        var levels = Enum.GetValues<LogLevel>().Where(l => l != LogLevel.None).ToArray();
+        while (!stoppingToken.IsCancellationRequested)
+        {
+            var level = levels[random.Next(levels.Length)];
+            var logger = random.Next(0, 2) switch
+            {
+                0 => loggerFactory.CreateLogger($"Sample.Logger{random.Next(1, 4)}"),
+                1 => loggerFactory.CreateLogger($"Sample.Group{random.Next(1,4)}.Logger{random.Next(1, 4)}"),
+                _ => loggerFactory.CreateLogger($"Sample.Group{random.Next(1,4)}.SubGroup{random.Next(1,4)}.Logger{random.Next(1, 4)}"),
+            };
+
+            logger.Log(level, "Worker running at: {Time} with log level: {LogLevel}", DateTime.Now, level);
+            await Task.Delay(random.Next(100, 2001), stoppingToken);
+        }
+    }
+}
