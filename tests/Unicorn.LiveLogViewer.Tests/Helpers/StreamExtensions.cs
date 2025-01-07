fbf0bc70d2ce4dd097d003f31228de090de9a966
Damien Braillard <contact@damien-braillard.ch>
2025-01-07 11:01:46 +0100

    Add new testing helpers
    * Some extensions methods on Stream and String
    * An ASP.Net test server for integration testing
    * A package to better test web responses

---
+++ tests/Unicorn.LiveLogViewer.Tests/Helpers/StreamExtensions.cs
@@ -0,0 +1,25 @@
+﻿using System.IO;
+using System.Text;
+
+namespace Unicorn.LiveLogViewer.Tests.Helpers;
+
+/// <summary>
+/// Provides test extension methods on the <see cref="Stream"/> type.
+/// </summary>
+public static class StreamTestExtensions
+{
+    private static readonly Encoding Encoding = new UTF8Encoding(encoderShouldEmitUTF8Identifier: false);
+
+    /// <summary>
+    /// Converts a stream to an UTF8 string that contains the text read from the stream current position.
+    /// </summary>
+    /// <param name="stream">The stream to read into the returned string.</param>
+    /// <param name="dispose"><c>true</c> to dispose the stream (default); otherwise, <c>false</c>.</param>
+    /// <returns>A <see cref="string"/> that contains the text that could be read from the stream current position.</returns>
+    public static string ToUtf8String(this Stream stream, bool dispose = true)
+    {
+        using var reader = new StreamReader(stream, Encoding, leaveOpen: !dispose);
+        var str = reader.ReadToEnd();
+        return str;
+    }
+}
---
+++ tests/Unicorn.LiveLogViewer.Tests/Helpers/StringExtensions.cs
@@ -0,0 +1,18 @@
+﻿using System.Text;
+
+namespace Unicorn.LiveLogViewer.Tests.Helpers;
+
+/// <summary>
+/// Provides test extension methods on the <see cref="string"/> type.
+/// </summary>
+public static class StringTestExtensions
+{
+    private static readonly Encoding Encoding = new UTF8Encoding(encoderShouldEmitUTF8Identifier: false);
+
+    /// <summary>
+    /// Converts a string to a buffer containing the UTF8 binary representation of the string.
+    /// </summary>
+    /// <param name="str">The string to convert to a UTF-8 buffer .</param>
+    /// <returns>A buffer that contains the UTF-8 binary representation of the string.</returns>
+    public static byte[] ToUtf8Buffer(this string str) => Encoding.GetBytes(str);
+}
